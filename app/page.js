c4f2e89a88577d740b0f37e10460561ef24600f1
roverflow <roverflow5@gmail.com>
2023-11-06 11:01:52 +0530

    added timer

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import {
@@ -12,12 +12,43 @@ import checkAuth from "@/hooks/checkAuth";
 import getWorkOut from "@/hooks/getWorkOut";
 import { Loading, GeneralDisplay } from "@/components/GeneralComponents";
 import Link from "next/link";
+import { secondsToHMS } from "@/lib/utils";
 
 export default function Home() {
   const { user } = checkAuth();
   const { thisWorkOut, loading, currentWorkoutTemplate, userDoc } =
     getWorkOut(user);
   const [prevWorkout, setPrevWorkout] = useState(null);
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [workStartTime, setWorkStartTime] = useState(null);
+  const [workEndTime, setWorkEndTime] = useState(null);
+
+  const startWorkOut = () => {
+    setIsRunning(true);
+    setWorkStartTime(Date.now());
+  };
+
+  const stopAndSubmit = () => {
+    setIsRunning(false);
+    setWorkEndTime(Date.now());
+  };
+
+  useEffect(() => {
+    let intervalId;
+
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        setTime(time + 1);
+      }, 1000);
+    } else {
+      clearInterval(intervalId);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [isRunning, time]);
+
+  // return
   if (loading) return <Loading />;
   if (thisWorkOut === null || currentWorkoutTemplate === null) {
     return (
@@ -74,16 +105,36 @@ export default function Home() {
                 </div>
               </CardHeader>
               <CardContent>
+                <div className="w-full flex justify-between my-5">
+                  <div className="font-bold border-2 p-1 px-3 rounded">
+                    {secondsToHMS(time)}
+                  </div>
+                  {!isRunning ? (
+                    <button
+                      className={`bg-green-500 text-white font-bold p-2 rounded`}
+                      onClick={startWorkOut}
+                    >
+                      Start workout
+                    </button>
+                  ) : (
+                    <button
+                      className={`bg-red-500 text-white font-bold p-2 rounded`}
+                      onClick={stopAndSubmit}
+                    >
+                      Stop workout
+                    </button>
+                  )}
+                </div>
                 <div className="flex flex-col gap-4">
                   {thisWorkOut.data.programData[
                     userDoc.currentWorkoutTemplateIndex
                   ].exercises.map((exercise, index) => {
                     return (
-                      <Card className="w-full" key={index}>
-                        <CardHeader>
+                      <div className="w-full flex flex-col gap-3" key={index}>
+                        <div>
                           <CardTitle>{exercise.exerciseName}</CardTitle>
-                        </CardHeader>
-                        <CardContent>
+                        </div>
+                        <div className="border-2 px-2 py-1 rounded">
                           {exercise.sets.map((set, index) => {
                             return (
                               <div className="flex gap-2">
@@ -99,8 +150,8 @@ export default function Home() {
                               </div>
                             );
                           })}
-                        </CardContent>
-                      </Card>
+                        </div>
+                      </div>
                     );
                   })}
                 </div>
